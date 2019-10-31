1c45cc8427a5e952f5297b9d50d6518aa45c3498
Dcbaas <Dcbaas@users.noreply.github.com>
2019-10-31 15:34:01 -0400

    added gitignore. fleshed out adding new coordinates

--- VoronoiSolver.cpp
+++ VoronoiSolver.cpp
@@ -9,35 +9,29 @@
 
 Coordinate::Coordinate(const int x, const int y) : x(x), y(y) {}
 
-size_t Coordinate::findDistance(Coordinate& other){
+size_t Coordinate::findDistance(const Coordinate& other){
     return 0;
 }
 
-//bool operator<(Coordinate& other){
-    //size_t thisSize = this->x * this->y;
-    //size_t otherSize = other.x * other.y;
-    //return thisSize < otherSize;
-//}
-//bool operator<(Coordinate& lhs, Coordinate& rhs){
-    //return true;
-//}
 VoroniStructures::VoroniStructures(std::map<seed, Coordinate>& seedToLocationMap){
     this->seedToLocationMap = std::move(seedToLocationMap);
 }
 
 void VoroniStructures::addCoordinate(const int x, const int y){
     Coordinate coord(x, y);
-    std::pair<seed, Coordinate>* closestCoord = nullptr;
-    size_t closestDistance = ULLONG_MAX;
+    seed closestSeed{0};
+    size_t closestDistance{ULLONG_MAX};
     for(const auto& [key, value]: seedToLocationMap){
-        continue;
+        size_t calculatedDistance = coord.findDistance(value);
+
+        if(calculatedDistance < closestDistance){
+            closestDistance = calculatedDistance;
+            closestSeed = key;
+        }
     }
 
     VoroniStructures::structuresLock.lock();
-    coordinateToSeedMap.emplace(coord, closestCoord->first);
+    coordinateToSeedMap.emplace(coord, closestSeed);
     VoroniStructures::structuresLock.unlock();
 }
 
-//std::map<Coordinate, seed>& VoroniStructures::getCoordinateToSeedMap(){
-    //return coordinateToSeedMap;
-//}
--- VoronoiSolver.hpp
+++ VoronoiSolver.hpp
@@ -8,25 +8,30 @@ using seed = int;
 class Coordinate{
 public:
     Coordinate(const int x, const int y);
-    size_t findDistance(Coordinate& other);
+    size_t findDistance(const Coordinate& other);
     int x;
     int y;
 };
 
 struct coordCompare{
     bool operator()(const Coordinate& lhs, const Coordinate& rhs)const{
-        return true;
+        if(lhs.x != rhs.x){
+            return lhs.x < rhs.x;
+        }
+        return (lhs.x * lhs.y) < (rhs.x * rhs.y);
     }
 };
+
+using CoordinateSeedMap = std::map<Coordinate, seed, coordCompare>;
 
 class VoroniStructures{
 public:
     VoroniStructures(std::map<seed, Coordinate>& seedToLocationMap);
     void addCoordinate(const int x, const int y);
-    std::map<Coordinate, seed>& getCoordinateToSeedMap();
+    CoordinateSeedMap& getCoordinateToSeedMap()const;
 private:
     // This structure holds the entire bitmap and represents what seed a coordinate is closest to.
-    std::map<Coordinate, seed, coordCompare> coordinateToSeedMap;
+    CoordinateSeedMap coordinateToSeedMap;
 
     // This structre represents  the seeds and where they are located.
     std::map<seed, Coordinate> seedToLocationMap;
